854ca2ef592df02294b326774a1f0bc58e18e0aa
ImaniAN <32896144+ImaniAN@users.noreply.github.com>
2022-07-28 06:42:04 +0200

    Components Folder

--- sveltekit-tailwindcss/src/routes/__layout.svelte
+++ sveltekit-tailwindcss/src/routes/__layout.svelte
@@ -1,6 +1,4 @@
-<!--  src/routes/$layout.svelte -->
 <script>
-	import '../tailwind.css';
 	import '../app.css';
 </script>
 
--- sveltekit-tailwindcss/src/routes/index.svelte
+++ sveltekit-tailwindcss/src/routes/index.svelte
@@ -1,7 +1,12 @@
+<script>
+	import SignUpForm from '../components/SignUpForm.svelte';
+</script>
+
 <main>
 	<h1
 		class="text-red-700 container mx-auto my-6 max-w-lg text-2xl font-bold text-center md:text-3xl"
 	>
 		Skills Do work!!
 	</h1>
+	<SignUpForm />
 </main>
