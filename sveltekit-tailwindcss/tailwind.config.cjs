7c6309a8d9049110bf54975b6e044c63f3d71e1e
ImaniAN <32896144+ImaniAN@users.noreply.github.com>
2022-07-28 08:44:46 +0200

    Tailwind v3 upgrade

--- sveltekit-tailwindcss/tailwind.config.cjs
+++ sveltekit-tailwindcss/tailwind.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{svelte,html,css,js,ts}',],
-  purge: ['./src/**/*.svelte', './src/**/*.css'],
-  darkMode: false,
+  content: ['./src/**/*.{svelte,html,css,js}',],
   theme: {
     extend: {},
   },
