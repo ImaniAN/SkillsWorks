0ee1c6e0f618a11ea624e0910205f5996ff3a411
ImaniAN <32896144+ImaniAN@users.noreply.github.com>
2022-07-29 19:04:41 +0200

    File Structure tree

---
+++ sveltekit-tailwindcss/src/components/File Strructure Tree/profiles/recruiter/recruiter.svelte
@@ -0,0 +1,380 @@
+<div class="relative min-h-screen flex ">
+	<div
+		class="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0 bg-white"
+	>
+		<div
+			class="sm:w-1/2 xl:w-3/5 h-full hidden md:flex flex-auto items-end justify-end p-10 pr-20 overflow-hidden bg-blue-900 text-white bg-no-repeat bg-cover relative"
+			style="background-image: url(https://images.unsplash.com/photo-1543599538-a6c4f6cc5c05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80);"
+		>
+			<div class="absolute bg-gradient-to-b from-indigo-100 to-blue-600 opacity-75 inset-0 z-0" />
+			<div class="w-full z-10 mb-10">
+				<div class="sm:text-4xl xl:text-5xl  text-gray-800 font-bold leading-tight mb-6">
+					Reference: Call to action
+				</div>
+				<div class="sm:text-sm xl:text-md text-gray-900 font-normal">
+					What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting
+					industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when
+					an unknown printer took a galley of type and scrambled it to make a type specimen book it
+					has?
+				</div>
+			</div>
+			<!---remove custom style-->
+			<ul class="squares">
+				<li />
+				<li />
+				<li />
+				<li />
+				<li />
+				<li />
+				<li />
+				<li />
+				<li />
+				<li />
+			</ul>
+		</div>
+		<div
+			class="md:flex md:items-center md:justify-cente sm:w-auto md:h-full w-4/5 xl:w-2/5 p-8  md:p-10 lg:p-14 sm:rounded-lg md:rounded-none bg-white"
+		>
+			<div class="max-w-md w-full space-y-8">
+				<div class="mb-10 text-center">
+					<h3 class="font-semibold text-2xl text-gray-800">Sign Up</h3>
+					<p class="text-gray-500">Please register your account.</p>
+				</div>
+				<div class="flex">
+					<div class="w-full md:w-1/2 px-0.5 mb-6 md:mb-0">
+						<button
+							type="submit"
+							class="w-full flex items-center justify-center bg-red-500  hover:bg-red-400 text-gray-100 p-3  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500"
+						>
+							Google
+							<svg
+								class="w-4 ml-2"
+								fill="#fff"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+								><path
+									d="M11.99 13.9v-3.72h9.36c.14.63.25 1.22.25 2.05 0 5.71-3.83 9.77-9.6 9.77-5.52 0-10-4.48-10-10S6.48 2 12 2c2.7 0 4.96.99 6.69 2.61l-2.84 2.76c-.72-.68-1.98-1.48-3.85-1.48-3.31 0-6.01 2.75-6.01 6.12s2.7 6.12 6.01 6.12c3.83 0 5.24-2.65 5.5-4.22h-5.51v-.01Z"
+								/></svg
+							>
+						</button>
+					</div>
+					<div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+						<button
+							type="submit"
+							class="w-full flex items-center justify-center bg-blue-600  hover:bg-blue-500 text-gray-100 p-3  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500"
+						>
+							Facebook
+							<svg class="w-4 ml-2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+								><path
+									fill="#fff"
+									fill-rule="evenodd"
+									d="M9.945 22v-8.834H7V9.485h2.945V6.54c0-3.043 1.926-4.54 4.64-4.54 1.3 0 2.418.097 2.744.14v3.18h-1.883c-1.476 0-1.82.703-1.82 1.732v2.433h3.68l-.736 3.68h-2.944L13.685 22"
+								/></svg
+							>
+						</button>
+					</div>
+				</div>
+				<div class="flex items-center justify-center space-x-2 my-5">
+					<span class="h-px w-16 bg-gray-200" />
+					<span class="text-gray-500 font-normal text-center">or continue with</span>
+					<span class="h-px w-16 bg-gray-200" />
+				</div>
+				<form class="w-full max-w-lg">
+					<div class="flex flex-wrap -mx-3 mb-4">
+						<div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+							<label
+								class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+								for="grid-first-name"
+							>
+								First Name
+							</label>
+							<input
+								class="appearance-none block w-full bg-gray-100 text-gray-700 rounded py-3 px-4 mb-3 border border-gray-200 leading-tight focus:outline-none focus:bg-white focus:border-teal-300"
+								id="grid-first-name"
+								type="text"
+								placeholder="Jane"
+							/>
+						</div>
+						<div class="w-full md:w-1/2 px-3">
+							<label
+								class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+								for="grid-last-name"
+							>
+								Last Name
+							</label>
+							<input
+								class="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-teal-300"
+								id="grid-last-name"
+								type="text"
+								placeholder="Doe"
+							/>
+						</div>
+					</div>
+					<div class="flex flex-wrap -mx-3 mb-4">
+						<div class="w-full px-3">
+							<label
+								class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+								for="grid-email"
+							>
+								Email Address
+							</label>
+							<input
+								class="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-teal-300"
+								id="grid-email"
+								type="email"
+								placeholder="username@example.com"
+							/>
+						</div>
+					</div>
+					<div class="flex flex-wrap -mx-3 mb-4">
+						<div class="w-full px-3">
+							<label
+								class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+								for="grid-password"
+							>
+								Password
+							</label>
+							<input
+								class="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-teal-300"
+								id="grid-password"
+								type="password"
+								placeholder="******************"
+							/>
+						</div>
+					</div>
+					<div class="flex flex-wrap -mx-3 mb-4">
+						<div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+							<label
+								class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+								for="grid-city"
+							>
+								City
+							</label>
+							<input
+								class="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-teal-300"
+								id="grid-city"
+								type="text"
+								placeholder="Durban"
+							/>
+						</div>
+						<div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+							<label
+								class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+								for="grid-province"
+							>
+								Province
+							</label>
+							<div class="relative">
+								<select
+									class="block appearance-none w-full bg-gray-100 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-teal-300"
+									id="province"
+								>
+									<option value="" disabled selected hidden>Select...</option>
+									<option>Gauteng</option>
+									<option>KwaZulu-Natal</option>
+									<option>Western Cape</option>
+									<option>Eastern Cape</option>
+									<option>Free State</option>
+									<option>Limpopo</option>
+									<option>Mpumalanga</option>
+									<option>Northern Cape</option>
+									<option>North West</option>
+								</select>
+								<div
+									class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+								>
+									<svg
+										class="fill-current h-4 w-4"
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 20 20"
+									>
+										<path
+											d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+										/>
+									</svg>
+								</div>
+							</div>
+						</div>
+						<div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+							<label
+								class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+								for="grid-zip"
+							>
+								Zip
+							</label>
+							<input
+								class="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-teal-300"
+								id="grid-zip"
+								type="text"
+								placeholder="xxxx"
+							/>
+						</div>
+					</div>
+					<div class="space-y-6 mt-6">
+						<div class="flex items-center justify-between">
+							<div class="flex items-center">
+								<input
+									id="remember_me"
+									name="remember_me"
+									type="checkbox"
+									class="h-4 w-4 bg-blue-500 focus:ring-blue-400 border-gray-300 rounded"
+								/>
+								<label for="remember_me" class="ml-2 block text-sm text-gray-800">
+									Remember me
+								</label>
+							</div>
+							<div class="text-sm text-right">
+								<a href="#" class="text-blue-300 hover:text-blue-500 "> Forgot your password? </a>
+								<!-- TODORemeber to href to the Forgot your password Pages -->
+							</div>
+						</div>
+						<div>
+							<button
+								type="submit"
+								class="w-full flex justify-center bg-blue-300  hover:bg-blue-500 text-gray-100 p-3  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500"
+							>
+								Sign in
+							</button>
+						</div>
+						<div class="flex items-center justify-center space-x-2 my-5">
+							<span class="h-px w-16 bg-gray-200" />
+							<span class="text-gray-500 font-normal text-center">OR</span>
+							<span class="h-px w-16 bg-gray-200" />
+						</div>
+						<div>
+							<button
+								type="submit"
+								class="w-full flex justify-center bg-green-300  hover:bg-green-500 text-gray-100 p-3  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500"
+							>
+								Sign Up
+							</button>
+						</div>
+					</div>
+				</form>
+
+				<div class="pt-6 text-center text-gray-400 text-xs">
+					<span>
+						Copyright © 2021-2022
+						<a
+							href="https://github.com/Fathom-Growth-Solutions"
+							rel=""
+							target="_blank"
+							title="Fathom Growth Solutions"
+							class="text-blue-300 hover:text-blue-500 ">Fathom Growth Solutions</a
+						></span
+					>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- component -->
+<style>
+	/*remove custom style*/
+	.squares {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+	}
+	.squares li {
+		position: absolute;
+		display: block;
+		list-style: none;
+		width: 20px;
+		height: 20px;
+		background: #93c5fd;
+		animation: animate 25s linear infinite;
+		bottom: -150px;
+	}
+	.squares li:nth-child(1) {
+		left: 25%;
+		width: 80px;
+		height: 80px;
+		animation-delay: 0s;
+	}
+
+	.squares li:nth-child(2) {
+		left: 10%;
+		width: 20px;
+		height: 20px;
+		animation-delay: 2s;
+		animation-duration: 12s;
+	}
+
+	.squares li:nth-child(3) {
+		left: 70%;
+		width: 20px;
+		height: 20px;
+		animation-delay: 4s;
+	}
+
+	.squares li:nth-child(4) {
+		left: 40%;
+		width: 60px;
+		height: 60px;
+		animation-delay: 0s;
+		animation-duration: 18s;
+	}
+
+	.squares li:nth-child(5) {
+		left: 65%;
+		width: 20px;
+		height: 20px;
+		animation-delay: 0s;
+	}
+
+	.squares li:nth-child(6) {
+		left: 75%;
+		width: 110px;
+		height: 110px;
+		animation-delay: 3s;
+	}
+
+	.squares li:nth-child(7) {
+		left: 35%;
+		width: 150px;
+		height: 150px;
+		animation-delay: 7s;
+	}
+
+	.squares li:nth-child(8) {
+		left: 50%;
+		width: 25px;
+		height: 25px;
+		animation-delay: 15s;
+		animation-duration: 45s;
+	}
+
+	.squares li:nth-child(9) {
+		left: 20%;
+		width: 15px;
+		height: 15px;
+		animation-delay: 2s;
+		animation-duration: 35s;
+	}
+
+	.squares li:nth-child(10) {
+		left: 85%;
+		width: 150px;
+		height: 150px;
+		animation-delay: 0s;
+		animation-duration: 11s;
+	}
+	@keyframes animate {
+		0% {
+			transform: translateY(0) rotate(0deg);
+			opacity: 1;
+			border-radius: 0;
+		}
+
+		100% {
+			transform: translateY(-1000px) rotate(720deg);
+			opacity: 0;
+			border-radius: 50%;
+		}
+	}
+</style>
