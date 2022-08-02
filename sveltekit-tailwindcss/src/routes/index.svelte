b1db784ee50829036d0e17caa1ad3fb8b2e3d0e8
ImaniAN <32896144+ImaniAN@users.noreply.github.com>
2022-08-02 02:36:50 +0200

    Page and component additions

--- sveltekit-tailwindcss/src/routes/index.svelte
+++ sveltekit-tailwindcss/src/routes/index.svelte
@@ -1,6 +1,27 @@
 <script>
 	// import Sign_up_form from '../components/onboarding/Sign_up_form.svelte';
-	import Log_in_form from '../components/onboarding/Log_in_form.svelte';
+	// import Log_in_form from '../components/onboarding/Log_in_form.svelte';
+	// import Navbar from '../components/comps/Navbar.svelte';
+	// import Faq from '../components/about_us/Faq.svelte';
+	// import Footer from '../components/comps/Footer.svelte';
+	// import Menu from '../components/comps/extras/Menu.svelte';
+	// import Reviews from '../components/comps/extras/Reviews.svelte';
+	// import Tab_pills from '../components/comps/extras/Tab_pills.svelte';
+	// import Testimonial from '../components/comps/extras/Testimonial.svelte';
+	// import Pop_cart from '../components/comps/extras/Pop_cart.svelte';
+	// import Checkout from '../components/comps/extras/Checkout.svelte';
+	// import Sub_plan_card from '../components/comps/extras/Sub_plan_card.svelte';
+	// import Grouped_buttons from '../components/comps/extras/Grouped_buttons.svelte';
+	// import Dropdown from '../components/comps/extras/Dropdown.svelte';
+	// import Empty_state from '../components/comps/extras/Empty_state.svelte';
+	// import Nav_search from '../components/profiles/ui/Nav_search.svelte';
+	// import Inputs from '../components/comps/extras/Inputs.svelte';
+	// import Pagination from '../components/comps/extras/Pagination.svelte';
+	// import Nav_side_small from '../components/profiles/ui/Nav_side_small.svelte';
+	// import Nav_side from '../components/profiles/ui/Nav_side.svelte';
+	// import Accordion_menu from '../components/comps/extras/menus/Accordion_menu.svelte';
+	// import Split_with_headings from '../components/comps/extras/menus/Split_with_headings.svelte';
+	// import Branded_accent from '../components/comps/extras/menus/Branded_accent.svelte';
 </script>
 
 <main>
@@ -10,5 +31,26 @@
 		Skills Do work!!
 	</h1>
 	<!-- <Sign_up_form /> -->
-	<Log_in_form />
+	<!-- <Log_in_form /> -->
+	<!-- <Navbar /> -->
+	<!-- <Faq /> -->
+	<!-- <Footer /> -->
+	<!-- <Menu /> -->
+	<!-- <Reviews /> -->
+	<!-- <Tab_pills /> -->
+	<!-- <Testimonial /> -->
+	<!-- <Pop_cart /> -->
+	<!-- <Checkout /> -->
+	<!-- <Sub_plan_card /> -->
+	<!-- <Grouped_buttons /> -->
+	<!-- <Dropdown /> -->
+	<!-- <Empty_state /> -->
+	<!-- <Nav_search /> -->
+	<!-- <Inputs /> -->
+	<!-- <Pagination /> -->
+	<!-- <Nav_side_small /> -->
+	<!-- <Nav_side /> -->
+	<!-- <Accordion_menu /> -->
+	<!-- <Split_with_headings /> -->
+	<!-- <Branded_accent /> -->
 </main>
