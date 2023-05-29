c72715aee3e08b4f4285d99e3971618501541d6f
Imani 'K!ΠG' Niyigena <imanix8@gmail.com>
2023-05-29 15:16:31 +0200

    Serch menu comps try-out

--- SkillsWorks/src/components/profiles/ui/Jobs_search.svelte
+++ SkillsWorks/src/components/profiles/ui/Jobs_search.svelte
@@ -47,6 +47,85 @@
 		</div>
 		<!--END:()-->
 
+		<!-- component -->
+		<section class="container px-4 mx-auto">
+			<div class="mt-6 md:flex md:items-center md:justify-between">
+				<div
+					class="inline-flex overflow-hidden bg-white border divide-x rounded-lg dark:bg-gray-900 rtl:flex-row-reverse dark:border-gray-700 dark:divide-gray-700"
+				>
+					<button
+						class="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 bg-gray-100 sm:text-sm dark:bg-gray-800 dark:text-gray-300"
+					>
+						View all
+					</button>
+
+					<button
+						class="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
+					>
+						Monitored
+					</button>
+
+					<button
+						class="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
+					>
+						Unmonitored
+					</button>
+				</div>
+
+				<div class="flex items-center mt-4 gap-x-3">
+					<button
+						class="flex items-center justify-center w-1/2 tracking-wide transition-colors duration-200 shrink-0 sm:w-auto gap-x-2 dark:hover:bg-blue-50 dark:bg-gray-800 text-slate-50 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:text-indigo-900 focus:bg-indigo-50 bg-slate-600 hover:border-2 hover:border-dashed hover:border-teal-600 hover:text-indigo-900 hover:bg-indigo-50 px-3 py-2 rounded-md text-sm font-medium"
+					>
+						<svg
+							width="20"
+							height="20"
+							viewBox="0 0 20 20"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<g clip-path="url(#clip0_3098_154395)">
+								<path
+									d="M13.3333 13.3332L9.99997 9.9999M9.99997 9.9999L6.66663 13.3332M9.99997 9.9999V17.4999M16.9916 15.3249C17.8044 14.8818 18.4465 14.1806 18.8165 13.3321C19.1866 12.4835 19.2635 11.5359 19.0351 10.6388C18.8068 9.7417 18.2862 8.94616 17.5555 8.37778C16.8248 7.80939 15.9257 7.50052 15 7.4999H13.95C13.6977 6.52427 13.2276 5.61852 12.5749 4.85073C11.9222 4.08295 11.104 3.47311 10.1817 3.06708C9.25943 2.66104 8.25709 2.46937 7.25006 2.50647C6.24304 2.54358 5.25752 2.80849 4.36761 3.28129C3.47771 3.7541 2.70656 4.42249 2.11215 5.23622C1.51774 6.04996 1.11554 6.98785 0.935783 7.9794C0.756025 8.97095 0.803388 9.99035 1.07431 10.961C1.34523 11.9316 1.83267 12.8281 2.49997 13.5832"
+									stroke="currentColor"
+									stroke-width="1.67"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+							</g>
+							<defs>
+								<clipPath id="clip0_3098_154395">
+									<rect width="20" height="20" fill="white" />
+								</clipPath>
+							</defs>
+						</svg>
+
+						<span>Import</span>
+					</button>
+
+					<button
+						class="flex items-center justify-center w-1/2 tracking-wide transition-colors duration-200 shrink-0 sm:w-auto gap-x-2 dark:hover:bg-blue-50 dark:bg-blue-600 text-slate-50 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:text-indigo-900 focus:bg-indigo-50 bg-slate-600 hover:border-2 hover:border-dashed hover:border-teal-600 hover:text-indigo-900 hover:bg-indigo-50 px-3 py-2 rounded-md text-sm font-medium"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-5 h-5"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+
+						<span>Add vendor</span>
+					</button>
+				</div>
+			</div>
+		</section>
+
 		<!--START:()-->
 		<div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
 			<!--START:()-->
