e2ebc4b2f61db472953c6d66d7476d178bbc0405
Imani Niyigena <imanix8@gmail.com>
2023-08-26 18:23:14 +0200

    Syncing Data With Supabase Tests

---
+++ SkillsWorks/src/components/data_testing/+page.server.js
@@ -0,0 +1,8 @@
+import { supabase } from "$lib/supabaseClient";
+
+export async function load() {
+  const { data } = await supabase.from("countries").select();
+  return {
+    countries: data ?? [],
+  };
+}
---
+++ SkillsWorks/src/components/data_testing/data.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Data_tings from '../data_testing/+page.svelte';
+	export let data;
+</script>
+
+\hgdhcfx
+<!-- <ul>
+	{#each data.countries as country}
+		<li>{country.name}</li>
+	{/each}
+</ul> -->
+
+<!-- <Data_tings name={data.name} countries={data.countries} /> -->
+
+<!-- {Data_tings.name} -->
+
+<!-- <main>
+	{#each data as item}
+		<p>{item.name}</p>
+	{/each}
+</main> -->
