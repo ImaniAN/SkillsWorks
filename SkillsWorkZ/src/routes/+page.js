e5e7eeb46695b68200a1a6f7ce419af8f6f55f14
Imani Niyigena <imanix8@gmail.com>
2023-08-27 19:40:02 +0200

    Svelte Demo

--- SkillsWorkZ/src/routes/+page.js
+++ SkillsWorkZ/src/routes/+page.js
@@ -10,4 +10,6 @@ export async function load() {
   return {
     skills_levels: data ?? [],
   };
-}
+}// since there's no dynamic data here, we can prerender
+// it so that it gets served as a static asset in production
+export const prerender = true;
