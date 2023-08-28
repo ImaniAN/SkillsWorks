4bb1eb4bf1a1ffa63ba1fa18930d769fa46b0a5e
Imani Niyigena <imanix8@gmail.com>
2023-08-28 23:28:18 +0200

    Requesting Data

--- SkillsWorkZ/src/routes/+page.js
+++ SkillsWorkZ/src/routes/+page.js
@@ -1,3 +1,13 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
+
+
+import { supabase } from "$lib/supabaseClient";
+
+export async function load() {
+  const { data } = await supabase.from("skills_levels").select();
+  return {
+    skills_levels: data ?? [],
+  };
+}
