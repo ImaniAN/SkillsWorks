c54cc61ccb5fa5291abae6c122d00512066497b3
Imani Niyigena <imanix8@gmail.com>
2023-08-29 16:48:04 +0200

    Vite and Tailwind Config

--- SkillsWorkZ/tailwind.config.js
+++ SkillsWorkZ/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
