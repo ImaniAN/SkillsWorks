4ca4d293e431dbfcb4b79b9afde2e926f316e74a
Imani Niyigena <imanix8@gmail.com>
2023-08-31 11:10:11 +0200

    Nav bar header additions

--- SkillsWorkZ/src/routes/Header.svelte
+++ SkillsWorkZ/src/routes/Header.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { USERDOMAIN } from './../../.svelte-kit/ambient.d.ts';
 	import { page } from '$app/stores';
 	import logo from '$lib/images/skillsworks-logo.svg';
 	import github from '$lib/images/sign-in.svg';
@@ -18,9 +19,7 @@
 			<li>
 				<a href="/"
 					><div class="group inline-block">
-						<button
-							class="outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32"
-						>
+						<button class="outline-none focus:outline-none rounded-sm flex items-center min-w-32">
 							<span class="pr-1 font-semibold flex-1">Home</span>
 						</button>
 					</div></a
@@ -29,9 +28,7 @@
 			<li>
 				<a href="/about"
 					><div class="group inline-block">
-						<button
-							class="outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32"
-						>
+						<button class="outline-none focus:outline-none rounded-sm flex items-center min-w-32">
 							<span class="pr-1 font-semibold flex-1">Jobs</span>
 							<span>
 								<svg
@@ -51,6 +48,7 @@
 				transition duration-150 ease-in-out origin-top min-w-32"
 						>
 							<li class="rounded-sm px-3 py-1 hover:bg-gray-100">Search Jobs</li>
+							<li class="rounded-sm px-3 py-1 hover:bg-gray-100">Job Alerts</li>
 							<li class="rounded-sm px-3 py-1 hover:bg-gray-100">Featured Jobs</li>
 							<li class="rounded-sm px-3 py-1 hover:bg-gray-100">Browse Jobs</li>
 							<li class="rounded-sm relative px-3 py-1 hover:bg-gray-100">
@@ -87,7 +85,6 @@
 									<li class="px-3 py-1 hover:bg-gray-100">Childcare</li>
 								</ul>
 							</li>
-							<li class="rounded-sm px-3 py-1 hover:bg-gray-100">Job Alerts</li>
 						</ul>
 					</div></a
 				>
@@ -95,9 +92,7 @@
 			<li>
 				<a href="/sverdle"
 					><div class="group inline-block">
-						<button
-							class="outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32"
-						>
+						<button class="outline-none focus:outline-none rounded-sm flex items-center min-w-32">
 							<span class="pr-1 font-semibold flex-1">Employer</span>
 							<span>
 								<svg
@@ -153,9 +148,7 @@
 			<li>
 				<a href="/sverdle"
 					><div class="group inline-block">
-						<button
-							class="outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32"
-						>
+						<button class="outline-none focus:outline-none rounded-sm flex items-center min-w-32">
 							<span class="pr-1 font-semibold flex-1">Job Seeker</span>
 							<span>
 								<svg
@@ -213,9 +206,37 @@
 			<li>
 				<a href="/sverdle"
 					><div class="group inline-block">
-						<button
-							class="outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32"
+						<button class="outline-none focus:outline-none rounded-sm flex items-center min-w-32">
+							<span class="pr-1 font-semibold flex-1">About us</span>
+							<span>
+								<svg
+									class="fill-current h-4 w-4 transform group-hover:-rotate-180
+							transition duration-150 ease-in-out"
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 20 20"
+								>
+									<path
+										d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+									/>
+								</svg>
+							</span>
+						</button>
+						<ul
+							class="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute
+				transition duration-150 ease-in-out origin-top min-w-32"
 						>
+							<li class="rounded-sm px-3 py-1 hover:bg-gray-100">Company Information</li>
+							<li class="rounded-sm px-3 py-1 hover:bg-gray-100">Contact Us</li>
+							<li class="rounded-sm px-3 py-1 hover:bg-gray-100">Privacy Policy</li>
+							<li class="rounded-sm px-3 py-1 hover:bg-gray-100">Privacy Policy</li>
+						</ul>
+					</div></a
+				>
+			</li>
+			<li>
+				<a href="/sverdle"
+					><div class="group inline-block">
+						<button class="outline-none focus:outline-none rounded-sm flex items-center min-w-32">
 							<span class="pr-1 font-semibold flex-1">User Support</span>
 							<span>
 								<svg
