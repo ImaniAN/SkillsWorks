27b06f560eb0dbb589fc0607b90b40d499a5860f
Imani Niyigena <imanix8@gmail.com>
2023-09-03 14:48:32 +0200

    onborading  potential comps

---
+++ SkillsWorkZ/src/components/onboarding/colors.js
@@ -0,0 +1,22 @@
+const colors = [
+	'orange',
+	'blue',
+	'green',
+	'red',
+	'yellow',
+	'pink',
+	'magenta',
+	'black',
+	'violet',
+	'navy',
+	'orangered',
+	'sky',
+	'purple',
+	'yellowgreen',
+	'salmon',
+	'crimson',
+	'hotpink',
+	'dogerblue',
+];
+
+export default colors;
