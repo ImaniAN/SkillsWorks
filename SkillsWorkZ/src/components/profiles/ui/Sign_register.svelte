aeb97cabd27bde3652b7935269fb6f98440c3955
Imani Niyigena <imanix8@gmail.com>
2023-09-03 14:24:14 +0200

    UI potential comps

---
+++ SkillsWorkZ/src/components/profiles/ui/Sign_register.svelte
@@ -0,0 +1,277 @@
+<!--START:()-->
+<div class="px-8 py-4 justify-center">
+	<!--START:()-->
+	<div class="relative min-h-screen flex">
+		<!--START:()-->
+		<div class="container mx-auto px-4 h-full">
+			<!--START:()-->
+			<div class="flex content-center items-center justify-center h-full">
+				<!--START:()-->
+				<div class="w-full px-4">
+					<!--START:()-->
+					<div
+						class="relative flex flex-col break-words w-full mb-6 shadow-lg rounded-lg bg-blue-200 border-0"
+					>
+						<!--START:()-->
+						<div class="rounded-t mb-0 px-6 py-6">
+							<!--START:()-->
+							<div class="text-center mb-3">
+								<h6 class="text-slate-500 text-sm font-bold">Sign in with</h6>
+							</div>
+							<!--START:()-->
+							<div class="btn-wrapper text-center">
+								<button
+									class="bg-indigo-50 active:bg-blue-50 text-slate-700 px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
+									type="button"
+								>
+									<img
+										alt="..."
+										class="w-5 mr-1"
+										src="https://dummyimage.com/305x305"
+									/>
+									Github
+								</button>
+								<button
+									class="bg-indigo-50 active:bg-blue-50 text-slate-700 px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
+									type="button"
+								>
+									<img
+										alt="..."
+										class="w-5 mr-1"
+										src="https://dummyimage.com/305x305"
+									/>
+									Google
+								</button>
+							</div>
+							<hr class="mt-6 border-b-1 border-blue-300" />
+						</div>
+						<!--START:()-->
+						<div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+							<!--START:()-->
+							<div class="text-slate-400 text-center mb-3 font-bold">
+								<small>Or sign in with credentials</small>
+							</div>
+							<form enctype="multipart/form-data" method="post">
+								<!--START:()-->
+								<div class="relative w-full mb-3">
+									<label
+										class="block uppercase text-slate-600 text-xs font-bold mb-2"
+										for="grid-email"
+									>
+										Email
+									</label>
+									<input
+										id="grid-email"
+										type="email"
+										class="border-0 px-3 py-3 placeholder-blue-300 text-slate-600 bg-indigo-50 rounded text-sm shadow focus:outline-none focus:ring w-full"
+										placeholder="Email"
+									/>
+								</div>
+
+								<!--START:()-->
+								<div class="relative w-full mb-3">
+									<label
+										class="block uppercase text-slate-600 text-xs font-bold mb-2"
+										for="grid-password"
+									>
+										Password
+									</label>
+									<input
+										id="grid-password"
+										type="password"
+										class="border-0 px-3 py-3 placeholder-blue-300 text-slate-600 bg-indigo-50 rounded text-sm shadow focus:outline-none focus:ring w-full"
+										placeholder="Password"
+									/>
+								</div>
+								<!--START:()-->
+								<div>
+									<label class="inline-flex items-center cursor-pointer">
+										<input
+											id="customCheckLogin"
+											type="checkbox"
+											class="bg-red-200 border-dashed border-2 rounded border-teal-400 text-teal-400 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+										/>
+										<span class="ml-2 text-sm font-semibold text-slate-600">
+											Remember me
+										</span>
+									</label>
+								</div>
+
+								<!--START:()-->
+								<div class="text-center mt-6">
+									<button
+										class="bg-blue-800 text-indigo-50 active:bg-blue-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
+										type="button"
+									>
+										Sign In
+									</button>
+								</div>
+							</form>
+						</div>
+					</div>
+					<!--START:()-->
+					<div class="flex flex-wrap mt-6 relative">
+						<!--START:()-->
+						<div class="w-1/2">
+							<!-- svelte-ignore a11y-invalid-attribute -->
+
+							<a href="#" class="text-slate-200">
+								<small>Forgot password?</small>
+							</a>
+						</div>
+						<!--START:()-->
+						<div class="w-1/2 text-right">
+							<!-- svelte-ignore a11y-invalid-attribute -->
+
+							<a href="#" class="text-slate-200">
+								<!-- svelte-ignore a11y-invalid-attribute -->
+								<!-- <a use:link href="/auth/register" class="text-slate-200"> -->
+								<small>Create new account</small>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--START:()-->
+	<div class="relative min-h-screen flex">
+		<!--START:()-->
+		<div class="container mx-auto px-4 h-full">
+			<!--START:()-->
+			<div class="flex content-center items-center justify-center h-full">
+				<!--START:()-->
+				<div class="w-full px-4">
+					<!--START:()-->
+					<div
+						class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blue-200 border-0"
+					>
+						<!--START:()-->
+						<div class="rounded-t mb-0 px-6 py-6">
+							<!--START:()-->
+							<div class="text-center mb-3">
+								<h6 class="text-slate-500 text-sm font-bold">Sign up with</h6>
+							</div>
+							<!--START:()-->
+							<div class="btn-wrapper text-center">
+								<button
+									class="bg-indigo-50 active:bg-blue-50 text-slate-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center text-xs"
+									type="button"
+								>
+									<img
+										alt="..."
+										class="w-5 mr-1"
+										src="https://dummyimage.com/305x305"
+									/>
+									Github
+								</button>
+								<button
+									class="bg-indigo-50 active:bg-blue-50 text-slate-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center text-xs"
+									type="button"
+								>
+									<img
+										alt="..."
+										class="w-5 mr-1"
+										src="https://dummyimage.com/305x305"
+									/>
+									Google
+								</button>
+							</div>
+							<hr class="mt-6 border-b-1 border-blue-300" />
+						</div>
+						<!--START:()-->
+						<div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+							<!--START:()-->
+							<div class="text-slate-400 text-center mb-3 font-bold">
+								<small>Or sign up with credentials</small>
+							</div>
+							<form enctype="multipart/form-data" method="post">
+								<!--START:()-->
+								<div class="relative w-full mb-3">
+									<label
+										class="block uppercase text-slate-600 text-xs font-bold mb-2"
+										for="grid-name"
+									>
+										Name
+									</label>
+									<input
+										id="grid-name"
+										type="email"
+										class="border-0 px-3 py-3 placeholder-blue-300 text-slate-600 bg-indigo-50 rounded text-sm shadow focus:outline-none focus:ring w-full"
+										placeholder="Name"
+									/>
+								</div>
+
+								<!--START:()-->
+								<div class="relative w-full mb-3">
+									<label
+										class="block uppercase text-slate-600 text-xs font-bold mb-2"
+										for="grid-email"
+									>
+										Email
+									</label>
+									<input
+										id="grid-email"
+										type="email"
+										class="border-0 px-3 py-3 placeholder-blue-300 text-slate-600 bg-indigo-50 rounded text-sm shadow focus:outline-none focus:ring w-full"
+										placeholder="Email"
+									/>
+								</div>
+
+								<!--START:()-->
+								<div class="relative w-full mb-3">
+									<label
+										class="block uppercase text-slate-600 text-xs font-bold mb-2"
+										for="grid-password"
+									>
+										Password
+									</label>
+									<input
+										id="grid-password"
+										type="password"
+										class="border-0 px-3 py-3 placeholder-blue-300 text-slate-600 bg-indigo-50 rounded text-sm shadow focus:outline-none focus:ring w-full"
+										placeholder="Password"
+									/>
+								</div>
+
+								<!--START:()-->
+								<div>
+									<label class="inline-flex items-center cursor-pointer">
+										<input
+											id="customCheckLogin"
+											type="checkbox"
+											class="bg-red-200 border-dashed border-2 rounded border-teal-400 text-teal-400 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+										/>
+										<span class="ml-2 text-sm font-semibold text-slate-600">
+											I agree with the
+											<!-- svelte-ignore a11y-invalid-attribute -->
+											<a
+												href="#pablo"
+												on:click={(e) => e.preventDefault()}
+												class="text-red-500"
+											>
+												Privacy Policy
+											</a>
+										</span>
+									</label>
+								</div>
+
+								<!--START:()-->
+								<div class="text-center mt-6">
+									<button
+										class="bg-blue-800 text-indigo-50 active:bg-blue-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
+										type="button"
+									>
+										Create Account
+									</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--END:()-->
+</div>
+<!--END:()-->
