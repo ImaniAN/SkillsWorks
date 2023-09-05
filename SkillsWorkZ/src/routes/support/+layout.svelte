c864a4df659db84fae9b46527a3846591ef193fb
Imani Niyigena <imanix8@gmail.com>
2023-09-05 10:42:03 +0200

    Main content screen adjustment

--- SkillsWorkZ/src/routes/support/+layout.svelte
+++ SkillsWorkZ/src/routes/support/+layout.svelte
@@ -1,12 +1,15 @@
 <script>
-	import Testimonial from './../../components/about/Ts_and_cs.svelte';
+	import Testimonial from './../../components/about/Aboutus.svelte';
 	// import Header from './../../components/home/Header.svelte';
 	// import Landing from './../../components/onboarding/Landing.svelte';
 	// import Scroll_to_top from './../../components/comps/mock/Scroll_to_top.svelte';
 </script>
 
-<div class="">
-	jhfjcg
-	<Testimonial />
-	<!-- <Scroll_to_top /> -->
-</div>
+<main>
+	<slot />
+	<div class="">
+		jhfjcg
+		<Testimonial />
+		<!-- <Scroll_to_top /> -->
+	</div>
+</main>
