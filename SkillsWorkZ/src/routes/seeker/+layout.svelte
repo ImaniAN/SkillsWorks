9c8d53c766205d7f1932b64a817c1a8d009d7475
Imani Niyigena <imanix8@gmail.com>
2023-09-13 10:39:57 +0200

    highest level route config for seeker route

---
+++ SkillsWorkZ/src/routes/seeker/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Seeker from './../../components/seeker/seeker.svelte';
+</script>
+
+<svelte:head>
+	<title>Seeker</title>
+	<meta name="description" content="How to play Sverdle" />
+</svelte:head>
+
+<main>
+	<slot />
+	<div class="">
+		<Seeker />
+	</div>
+</main>
