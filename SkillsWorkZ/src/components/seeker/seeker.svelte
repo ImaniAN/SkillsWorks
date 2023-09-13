a3f609eef68fdbffa38ff2af08128c9ed7882451
Imani Niyigena <imanix8@gmail.com>
2023-09-13 10:39:26 +0200

    comp render for route

---
+++ SkillsWorkZ/src/components/seeker/seeker.svelte
@@ -0,0 +1,9 @@
+<!-- This comp is for showing the index/home/base url/ aka index0 -->
+<script>
+	import Search_menu from './../../components/comps/menus/Search_menu.svelte';
+</script>
+
+<!--START:()-->
+<div class="justify-center">
+	<Search_menu />
+</div>
