549df46b286e6f354a4d675227a4f2a8701bcc12
Imani Niyigena <imanix8@gmail.com>
2023-09-13 10:52:29 +0200

    Prod error fixn - "supabase" not being exported

--- SkillsWorks/src/components/data_testing/+page.server.js
+++
@@ -1,8 +0,0 @@
-import { supabase } from "$lib/supabaseClient";
-
-export async function load() {
-  const { data } = await supabase.from("countries").select();
-  return {
-    countries: data ?? [],
-  };
-}
--- SkillsWorks/src/lib/supabaseClient.js
+++ SkillsWorks/src/lib/supabaseClient.js
@@ -2,4 +2,4 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://pqbwxpevyfbndjzmpqmz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxYnd4cGV2eWZibmRqem1wcW16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk3MzU4MDYsImV4cCI6MTk3NTMxMTgwNn0.xbVA6yiYVnjj6cfJw8lv2oR-Nj_He0ygFLl9J-0LumA')
+export const supabase = createClient('https://pqbwxpevyfbndjzmpqmz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxYnd4cGV2eWZibmRqem1wcW16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk3MzU4MDYsImV4cCI6MTk3NTMxMTgwNn0.xbVA6yiYVnjj6cfJw8lv2oR-Nj_He0ygFLl9J-0LumA')
